d86b3567846b8921857ebc8602d583d68fc344ea
Michael Liu <mike666234@gmail.com>
2020-01-22 03:59:58 -0800

    Add BASH shebang to top of shell script

--- grailed_run.sh
+++ grailed_run.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 PATH=/home/ubuntu/.local/bin:/home/ubuntu/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 python3 grailed_scraper.py && python3 grailed_comparer.py && python3 grailed_send_email.py
