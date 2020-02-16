962e9eeb3f2005de767c6a50cb77188b75b265aa
Michael Liu <mike666234@gmail.com>
2020-02-16 14:34:04 -0800

    Rename emailer script

--- grailed_run.sh
+++ grailed_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PATH=/home/ubuntu/.local/bin:/home/ubuntu/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 cd /home/ubuntu/grailed-notifier/
-python3 grailed_scraper.py && python3 grailed_comparer.py && python3 grailed_send_email.py
+python3 grailed_scraper.py && python3 grailed_comparer.py && python3 grailed_emailer.py
